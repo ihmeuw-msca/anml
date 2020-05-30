ca5fc14a8be4395d1aa045290196868da8c9104d
jizezhang <jizez@uw.edu>
2020-05-30 15:22:34 -0700

    added sample base/composite class for testing purpose

--- src/placeholder/solvers/base.py
+++ src/placeholder/solvers/base.py
@@ -10,22 +10,21 @@ class ScipyOpt(Solver):
 
     def fit(self, data, x_init=None, options=None):
         if x_init is None:
-            if self._model.x_init is not None:
-                x_init = self._model.x_init 
+            if self.model.x_init is not None:
+                x_init = self.model.x_init 
             else:
                 raise ValueError('No initial value for x.')
         result = sciopt.minimize(
-            fun=lambda x: self._model.objective(x, data),
+            fun=lambda x: self.model.objective(x, data),
             x0=x_init,
-            jac=lambda x: self._model.gradient(x, data),
+            jac=lambda x: self.model.gradient(x, data),
             bounds=self._model.bounds,
             options=options if options is not None else self.options,
         )
-        print(result.success)
         self.success = result.success
         self.x_opt = result.x
         self.fun_val_opt = result.fun
         self.status = result.message
 
     def predict(self, **kwargs):
-        return self._model.predict(self.x_opt, **kwargs)
+        return self.model.predict(self.x_opt, **kwargs)
---
+++ src/placeholder/solvers/composite.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from placeholder.solvers.interface import CompositeSolver
+
+class MultipleInitializations(CompositeSolver):
+
+    def __init__(self, sample_fun, solver=None):
+        super().__init__()
+        self.sample_fun = sample_fun
+
+    def fit(self, data, x_init=None, options=None):
+        if self.assert_solvers_defined() is True:
+            if len(self.solvers) > 1:
+                raise RuntimeError('Only implemented for single solver.')
+            xs_init = self.sample_fun(x_init)
+            fun_vals = []
+            xs_opt = []
+            for x in xs_init:
+                self.solvers[0].fit(data, x, options=options)
+                fun_vals.append(self.solvers[0].fun_val_opt)
+                xs_opt.append(self.solvers[0].x_opt)
+
+            self.x_opt = xs_opt[np.argmin(fun_vals)]
+            self.fun_val_opt.append(np.min(fun_vals))
+
+    def predict(self, **kwargs):
+        return self.solvers[0].predict(self.x_opt, **kwargs)

2993f03620892f17628a2f6f0f57b4d6bcf159ae
jizezhang <jizez@uw.edu>
2020-06-12 16:28:50 -0700

    modified test

--- tests/parameter/test_likelihood.py
+++ tests/parameter/test_likelihood.py
@@ -34,7 +34,7 @@ def test_likelihood_nll():
 
 @pytest.mark.parametrize("x", [-1, 0, 1])
 @pytest.mark.parametrize("mean", [-1, 0, 1])
-@pytest.mark.parametrize("std", list(np.random.randn(2)))
+@pytest.mark.parametrize("std", np.random.rand(3) + 0.1)
 def test_gaussian_likelihood(x, mean, std):
     np.testing.assert_almost_equal(
         GaussianLikelihood._likelihood(vals=[x], parameters=[[mean], [std]]),
@@ -50,8 +50,4 @@ def test_gaussian_likelihood_array():
     np.testing.assert_array_almost_equal(
         GaussianLikelihood._neg_log_likelihood(vals=np.array([0., 1., 2.]), parameters=[[0], [1]]),
         -np.log(norm.pdf(x=np.array([0., 1., 2.])))
-    )
-    np.testing.assert_array_almost_equal(
-        GaussianLikelihood._neg_log_likelihood(vals=[0., 0.], parameters=[[0., 1.], [1.0, 1.0]]),
-        -np.log(norm.pdf(x=0., loc=[0., 1.]))
     )

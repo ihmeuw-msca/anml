0280d991e2cb24f0c23dc324a75c3afbd72494aa
jizezhang <jizez@uw.edu>
2020-06-19 23:59:13 -0700

    Add param processor (#20)
    * moved some helper functions to utils
    
    * handle no re case
    
    * added processor for no re case

--- src/anml/parameter/processors.py
+++ src/anml/parameter/processors.py
@@ -7,45 +7,7 @@ from typing import Callable, Optional
 from anml.parameter.parameter import ParameterSet
 from anml.parameter.variables import Variable
 from anml.parameter.spline_variable import Spline
-from anml.parameter.utils import build_re_matrix
-
-
-def collect_blocks(
-    param_set: ParameterSet, 
-    attr_name: str, 
-    build_func: Optional[str] = None, 
-    should_include: Optional[Callable] = lambda x: True,
-    reset_params: Optional[bool] = False,
-    inputs: Optional[pd.DataFrame] = None,
-):
-    if reset_params:
-        param_set.reset()
-    
-    blocks = []
-    for parameter in param_set.parameters:
-        for variable in parameter.variables:
-            if should_include(variable):
-                if build_func is not None:
-                    func = getattr(variable, build_func)
-                    if inputs is not None:
-                        func(inputs)
-                    else:
-                        func()
-                blocks.append(getattr(variable, attr_name))
-    
-    return blocks
-
-
-def collect_priors(priors):
-    def prior_fun(x):
-        s = 0
-        val = 0.0
-        for prior in priors:
-            x_dim = prior.x_dim
-            val += prior.error_value(x[s: s + x_dim])
-            s += x_dim
-        return val 
-    return prior_fun
+from anml.parameter.utils import build_re_matrix, collect_priors, collect_blocks
 
 
 def combine_constraints(constr_matrix: np.ndarray, constr_lb: np.ndarray, constr_ub: np.ndarray):
@@ -67,7 +29,8 @@ def _collect_commons(param_set, df):
     # ---- collecting all kinds of blocks needed ----
     # design matrices ... 
     design_mat_blocks = collect_blocks(param_set, 'design_matrix', 'build_design_matrix', inputs=df)
-    design_mat_re_blocks = collect_blocks(param_set, 'design_matrix_re', 'build_design_matrix_re', should_include=has_re, inputs=df)
+    if param_set.num_re_var > 0:
+        design_mat_re_blocks = collect_blocks(param_set, 'design_matrix_re', 'build_design_matrix_re', should_include=has_re, inputs=df)
     
     # bounds ...
     lbs_fe = collect_blocks(param_set, 'lb_fe', 'build_bounds_fe')
@@ -82,20 +45,27 @@ def _collect_commons(param_set, df):
     fe_priors = collect_blocks(param_set, 'fe_prior')
 
     # build matrix that pads gammas to have length Z.shape[1]
-    re_var_diag = []
-    for block in design_mat_re_blocks:
-        re_var_diag.append(np.ones((block.shape[1], 1)))
-    re_var_diag_matrix = block_diag(*re_var_diag)   
+    if param_set.num_re_var > 0:
+        re_var_diag = []
+        for block in design_mat_re_blocks:
+            re_var_diag.append(np.ones((block.shape[1], 1)))
+        re_var_diag_matrix = block_diag(*re_var_diag)
+    else:
+        re_var_diag_matrix = None
 
     # ---- combining blocks -----
     design_matrix = np.hstack(design_mat_blocks)
-    design_matrix_re = np.hstack(design_mat_re_blocks)
+    if param_set.num_re_var > 0:
+        design_matrix_re = np.hstack(design_mat_re_blocks)
+    else:
+        design_matrix_re = None
     constr_matrix, constr_lower_bounds, constr_upper_bounds = combine_constraints(constr_mat_blocks, constr_lbs, constr_ubs)
     # check dimensions ...
     assert design_matrix.shape[1] == param_set.num_fe
     assert constr_matrix.shape[1] == param_set.num_fe
     assert len(constr_lower_bounds) == len(constr_upper_bounds) == constr_matrix.shape[0] 
-    assert design_matrix_re.shape[1] == re_var_diag_matrix.shape[0] == param_set.num_re
+    if param_set.num_re_var > 0:
+        assert design_matrix_re.shape[1] == re_var_diag_matrix.shape[0] == param_set.num_re
 
     return design_matrix, design_matrix_re, re_var_diag_matrix, lbs_fe, ubs_fe, constr_matrix, constr_lower_bounds, constr_upper_bounds, fe_priors
 
@@ -240,3 +210,30 @@ def process_for_maximal(param_set, df):
     param_set.variable_names = fe_variables_names + re_variables_names
 
 
+def process_for_no_re(param_set, df):
+    # ----- collecting all blocks ------
+    (
+        param_set.design_matrix, 
+        param_set.design_matrix_re, 
+        param_set.re_var_diag_matrix,
+        lbs_fe, 
+        ubs_fe, 
+        param_set.constr_matrix_full, 
+        param_set.constr_lower_bounds_full, 
+        param_set.constr_upper_bounds_full, 
+        fe_priors
+    ) = _collect_commons(param_set, df)
+
+    param_set.lower_bounds_full = np.hstack(list(lbs_fe))
+    param_set.upper_bounds_full = np.hstack(list(ubs_fe))
+    param_set.prior_fun = collect_priors(fe_priors)
+
+    fe_variables_names = []
+    for parameter in param_set.parameters:
+        for variable in parameter.variables:
+            # remembering name of variable -- so that we know what each column in X corresponds to
+            var_name = parameter.param_name + '_' + variable.covariate
+            for i in range(variable.num_fe):
+                fe_variables_names.append(var_name + '_' + str(i))
+
+    param_set.variable_names = fe_variables_names

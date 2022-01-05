75a489647b97712d38ffdd426da40e0734c1fbb7
zhengp0 <zhengp@uw.edu>
2022-01-05 10:37:40 -0800

    Improve the error message

--- src/anml/data/validator.py
+++ src/anml/data/validator.py
@@ -12,23 +12,26 @@ class Validator(ABC):
     def __call__(self, key: str, value: NDArray):
         pass
 
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}()"
+
 
 class NoNans(Validator):
 
     def __call__(self, key: str, value: NDArray):
         if np.isnan(value).any():
-            raise ValueError(f"{key} contains nans.")
+            raise ValueError(f"Column '{key}' contains nans.")
 
 
 class Positive(Validator):
 
     def __call__(self, key: str, value: NDArray):
         if (value <= 0).any():
-            raise ValueError(f"{key} contains nonpositive numbers.")
+            raise ValueError(f"Column '{key}' contains nonpositive numbers.")
 
 
 class Unique(Validator):
 
     def __call__(self, key: str, value: NDArray):
         if len(np.unique(value, axis=0)) < value.shape[0]:
-            raise ValueError(f"{key} contains duplicated value or rows.")
+            raise ValueError(f"Column '{key}' contains duplicated values.")

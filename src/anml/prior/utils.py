d6171293657b7c53fac7042eec363fc97b2ee63f
zhengp0 <zhengp@uw.edu>
2022-01-17 10:23:11 -0800

    Add and test function combine_priors_params

--- src/anml/prior/utils.py
+++ src/anml/prior/utils.py
@@ -1,7 +1,9 @@
-from typing import List, Optional, Type
+from typing import List, Optional, Tuple, Type
 
 import anml
+import numpy as np
 from anml.prior.main import Prior
+from numpy.typing import NDArray
 
 
 def get_prior_type(prior_type: str) -> Type:
@@ -22,3 +24,18 @@ def filter_priors(priors: List[Prior],
         return prior.mat is None and is_prior_instance
 
     return list(filter(condition, priors))
+
+
+def combine_priors_params(priors: List[Prior]) -> Tuple[NDArray, Optional[NDArray]]:
+    if not all(isinstance(prior, Prior) for prior in priors):
+        raise TypeError("All prior in priors must be an instance of Prior.")
+    if len(priors) == 0:
+        raise ValueError("Priors must be a non-empty list of priors.")
+    if len(priors) == 1:
+        return priors[0].params, priors[0].mat
+    params = np.hstack([prior.params for prior in priors])
+    mat = np.vstack([
+        prior.mat if prior.mat is not None else np.identity(prior.shape[1])
+        for prior in priors
+    ])
+    return params, mat

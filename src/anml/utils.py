2ba7d0e19ffe49d1cb70ddb717676f0f901c81eb
zhengp0 <zhengp@uw.edu>
2022-01-19 22:16:09 -0800

    Remove the old script

--- src/anml/utils.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-def _check_list_consistency(x, y, Error):
-    if not (isinstance(x, List) and isinstance(y, List)):
-        raise Error(f"{x} and {y} must be passed in as lists.")
-    if not len(x) == len(y):
-        raise Error(f"{x.__name__} and {y.__name__} are not of the same length.")

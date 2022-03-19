a9d37699f5b49b654d7346437bcb3eeadb1df39b
zhengp0 <zhengp@uw.edu>
2022-03-18 21:51:15 -0700

    Simplify the matrix classes

--- src/anml/linalg/matrix.py
+++ src/anml/linalg/matrix.py
@@ -6,8 +6,8 @@ Matrix classes for consistent interface.
 """
 from __future__ import annotations
 
-from abc import ABC, abstractmethod, abstractproperty
-from typing import Any, Tuple, Union
+from abc import ABC, abstractmethod
+from typing import Protocol, Tuple, Union
 
 import numpy
 from numpy.typing import NDArray
@@ -15,23 +15,68 @@ from scipy.sparse import csc_matrix, csr_matrix, spdiags
 from scipy.sparse.linalg import spsolve
 
 
+class Array(Protocol):
+
+    @property
+    def T(self) -> Array:
+        """Transpose of the array."""
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        """Shape of the array."""
+
+    @property
+    def size(self) -> int:
+        """Size of the array, total number of elements."""
+
+    @property
+    def ndim(self) -> int:
+        """Number of dimensions of the array."""
+
+    def dot(self, x: Array) -> Array:
+        """Dot product with another array."""
+
+
+def asmatrix(data: Array) -> Matrix:
+    if isinstance(data, Matrix):
+        return data
+    if isinstance(data, numpy.ndarray):
+        return NumpyMatrix(data)
+    if isinstance(data, csr_matrix):
+        return CSRMatrix(data)
+    if isinstance(data, csc_matrix):
+        return CSCMatrix(data)
+    raise TypeError(f"Cannot convert {type(data)} to a matrix.")
+
+
 class Matrix(ABC):
 
-    @abstractproperty
+    def __init__(self, data: Array):
+        if data.ndim != 2:
+            raise ValueError("Matrix must have two dimensions")
+        self.data = data
+
+    @property
     def T(self) -> Matrix:
-        pass
+        return asmatrix(self.data.T)
 
-    @abstractproperty
+    @property
     def shape(self) -> Tuple[int, int]:
-        pass
+        return self.data.shape
 
-    @abstractproperty
+    @property
     def size(self) -> int:
-        pass
+        return self.data.size
 
-    @abstractmethod
+    @property
+    def ndim(self) -> int:
+        return self.data.ndim
+
     def dot(self, x: Union[NDArray, Matrix]) -> Union[NDArray, Matrix]:
-        pass
+        if isinstance(x, numpy.ndarray) and x.ndim == 1:
+            return self.data.dot(x)
+        x = asmatrix(x)
+        return asmatrix(self.data.dot(x.data))
 
     @abstractmethod
     def scale_rows(self, x: NDArray) -> Matrix:
@@ -49,54 +94,20 @@ class Matrix(ABC):
     def to_numpy(self) -> NDArray:
         pass
 
+    def __add__(self, x: Matrix) -> Matrix:
+        return asmatrix(self.data + x.data)
+
+    def __sub__(self, x: Matrix) -> Matrix:
+        return asmatrix(self.data - x.data)
+
     def __repr__(self) -> str:
         return f"{type(self).__name__}(shape={self.shape})"
-
-
-class SparseMatrix(Matrix):
-
-    @abstractproperty
-    def nnz(self) -> int:
-        pass
-
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}(shape={self.shape}, nnz={self.nnz})"
-
-
-def asmatrix(data: Any) -> Matrix:
-    if isinstance(data, numpy.ndarray):
-        return NumpyMatrix(data)
-    if isinstance(data, csr_matrix):
-        return CSRMatrix(data)
-    if isinstance(data, csc_matrix):
-        return CSCMatrix(data)
-    raise TypeError("Unknown data type, cannot convert to a matrix.")
 
 
 class NumpyMatrix(Matrix):
 
     def __init__(self, data: NDArray):
-        data = numpy.asarray(data)
-        if data.ndim != 2:
-            raise ValueError("Matrix must have two dimensions")
-        self.data = data
-
-    @property
-    def T(self) -> NumpyMatrix:
-        return NumpyMatrix(self.data.T)
-
-    @property
-    def shape(self) -> Tuple[int, int]:
-        return self.data.shape
-
-    @property
-    def size(self) -> int:
-        return self.data.size
-
-    def dot(self, x: Union[NDArray, Matrix]) -> Union[NDArray, Matrix]:
-        if isinstance(x, numpy.ndarray):
-            return self.data.dot(x)
-        return asmatrix(self.data.dot(x.data))
+        super().__init__(numpy.asarray(data))
 
     def scale_rows(self, x: NDArray) -> NumpyMatrix:
         return NumpyMatrix(x[:, numpy.newaxis] * self.data)
@@ -111,34 +122,10 @@ class NumpyMatrix(Matrix):
         return self.data.copy()
 
 
-class CSRMatrix(SparseMatrix):
+class CSRMatrix(Matrix):
 
     def __init__(self, data: csr_matrix):
-        if not isinstance(data, csr_matrix):
-            raise TypeError("Data of CSRMatrix must be an instance of "
-                            "csr_matrix.")
-        self.data = data
-
-    @property
-    def T(self) -> CSCMatrix:
-        return CSCMatrix(self.data.T)
-
-    @property
-    def shape(self) -> Tuple[int, int]:
-        return self.data.shape
-
-    @property
-    def size(self) -> int:
-        return self.data.size
-
-    @property
-    def nnz(self) -> int:
-        return self.data.nnz
-
-    def dot(self, x: Union[NDArray, Matrix]) -> Union[NDArray, Matrix]:
-        if isinstance(x, numpy.ndarray):
-            return self.data.dot(x)
-        return asmatrix(self.data.dot(x.data))
+        super().__init__(csr_matrix(data))
 
     def scale_rows(self, x: NDArray) -> CSRMatrix:
         return CSRMatrix(spdiags(x, 0, len(x), len(x)) * self.data)
@@ -155,34 +142,10 @@ class CSRMatrix(SparseMatrix):
         return self.data.toarray()
 
 
-class CSCMatrix(SparseMatrix):
+class CSCMatrix(Matrix):
 
     def __init__(self, data: csc_matrix):
-        if not isinstance(data, csc_matrix):
-            raise TypeError("Data of CSCMatrix must be an instance of "
-                            "csc_matrix.")
-        self.data = data
-
-    @property
-    def T(self) -> CSRMatrix:
-        return CSRMatrix(self.data.T)
-
-    @property
-    def shape(self) -> Tuple[int, int]:
-        return self.data.shape
-
-    @property
-    def size(self) -> int:
-        return self.data.size
-
-    @property
-    def nnz(self) -> int:
-        return self.data.nnz
-
-    def dot(self, x: Union[NDArray, Matrix]) -> Union[NDArray, Matrix]:
-        if isinstance(x, numpy.ndarray):
-            return self.data.dot(x)
-        return asmatrix(self.data.dot(x.data))
+        super().__init__(csc_matrix(data))
 
     def scale_rows(self, x: NDArray) -> CSCMatrix:
         result = self.data.copy()
